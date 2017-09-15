f6e2b20b00ef5712d7b9a518c2acf38415bdec85
wangqing <chinawangqing@163.com>
2017-09-15 14:50:50 +0800

    xiugai

--- hellow.js
+++ hellow.js
@@ -1,2 +1,19 @@
 console.log("wangqing")
 console.log("wangqing")
+
+var bianhao=parseInt(prompt("请输入汽油编号"));
+var sheng =parseFloat(prompt("请输入汽油升数"));
+if(bianhao==92){
+    if(sheng>=20){
+        var price=sheng*5.9;
+    }else{
+        var price =sheng*6;
+    }
+}else if (bianhao==97){
+    if(sheng>=30){
+        var price =sheng*9.95;
+    }else{
+        var price=sheng*7;
+    }
+}
+alert("价格是"+price);
